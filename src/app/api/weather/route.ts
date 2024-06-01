bfbc14f2cfc8975f021399e80faa67bb1703f6da
Shivansh-22866 <shivanshp0418@gmail.com>
2024-06-01 13:49:57 +0530

    aNextRequest, response: NextResponse

--- src/app/api/weather/route.ts
+++ src/app/api/weather/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(request) {
+export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url);
   const address = searchParams.get("address");
   const latitude = searchParams.get("lat");
